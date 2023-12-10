66562a2ed65825005fb2f01aee465e2302b37037
Tristan <tristanberthelotpichard@gmail.com>
2023-12-10 17:47:06 +0100

    MAX JTM <3

--- src/mains/main3.c
+++ src/mains/main3.c
@@ -5,14 +5,9 @@
 
 int main()
 {
-    t_calendar *cldr = NULL;
-    cldr = (t_calendar*) malloc(sizeof(t_calendar));
-    cldr->list_cnt = createEmptyCntList(SIZE);
-
-    insertContact(cldr->list_cnt, scanContact());
-    puts("scan\n");
-    displayAllLevelCnt(cldr->list_cnt);
-    puts("disp\n");
+    t_contact *contact;
+    contact = scanContact();
+    displayContact(contact);
     
     return 0;
 }
